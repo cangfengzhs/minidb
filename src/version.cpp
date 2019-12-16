2acd3b509a2f1ec90438b079e98f085f27029b95
cangfeng <cangfengzhs@qq.com>
2019-12-16 20:32:20 +0800

    Version 的sst改为多level

--- src/version.cpp
+++ src/version.cpp
@@ -17,14 +17,13 @@ namespace minidb {
         filemeta.remove_file();
     }
     Version::Version(minidb::ptr<minidb::LogWriter> log, minidb::ptr<minidb::LogWriter> imm_log,
-                     minidb::vec<minidb::ptr<minidb::SSTable>> sst_list, LogSeqNumber lsn, const std::string &db_name,
-                     int file_number, bool create) {
+                     SstSetList& sst_set_list, LogSeqNumber lsn, const std::string &db_name,
+                     int file_number, bool create):
+                     sst_set_list_(std::move(sst_set_list)),
+                     lsn_(lsn),
+                     log_(std::move(log)),
+                     pre_log_(std::move(imm_log)){
         filemeta.file_name=db_name + "/" + fn_fmt(file_number) + ".ver";
-        lsn_ = lsn;
-        log_ = std::move(log);
-        pre_log_ = std::move(imm_log);
-        sst_list_ = std::move(sst_list);
-        file_number = file_number;
         if (create) {
             BufWriter writer(db_name + "/" + fn_fmt(file_number) + ".ver", true, true);
             int x = log_ ? log_->file_number() : -1;
@@ -32,14 +31,20 @@ namespace minidb {
             log_debug("new version log:%d",x);
             x = pre_log_ ? pre_log_->file_number() : -1;
             writer.append(&x, 4);
-            x = sst_list_.size();
+            x=0;
+            for(const auto& sst_set:sst_set_list_){
+                x+=sst_set.size();
+            }
             writer.append(&x, 4);
             log_debug("new version sst cnt:%d",x);
-            for (int i = 0; i < sst_list_.size(); i++) {
-                x = sst_list_[i]->file_number();
-                log_debug("new version sst number:%d",x);
-                writer.append(&x, 4);
-                writer.append(&x, 1);//level标记 备用
+            for(int i=0;i<sst_set_list_.size();i++){
+                for(const auto& sst:sst_set_list_[i]){
+                    x = sst->file_number();
+                    log_debug("new version sst number:%d",x);
+                    writer.append(&x, 4);
+                    char level=(char)i;
+                    writer.append(&level, level);
+                }
             }
             writer.append(&lsn_, sizeof(LogSeqNumber));
             writer.append(reinterpret_cast<const char *>(&config::MAGIC), 8);
@@ -60,30 +65,28 @@ namespace minidb {
         if (pre_log) {
             lsn = max(lsn, pre_log->max_lsn());
         }
-        vec<ptr<SSTable>> sst_list = this->sst_list_;
-        for (auto s:edit->remove_sst_) {
-            for (auto iter = sst_list.begin(); iter != sst_list.end();) {
-                if (s == *iter) {
-                    iter = sst_list.erase(iter);
-                } else {
-                    iter++;
-                }
-            }
+        SstSetList sst_set_list;
+        for(int i=0;i<sst_set_list.size();i++){
+            std::set_difference(sst_set_list_[i].begin(),sst_set_list_[i].end(),
+                                edit->remove_sst_[i].begin(),edit->remove_sst_[i].end(),
+                                sst_set_list[i],sst_set_list[i].begin());
         }
-        for (auto s:edit->add_sst_) {
-            sst_list.push_back(s);
+        for(int i=0;i<sst_set_list.size();i++){
+            sst_set_list[i].insert(edit->add_sst_[i].begin(),edit->add_sst_[i].end());
         }
-        ptr<Version> new_version = make_ptr<Version>(log, pre_log, sst_list, lsn, db_name, file_number, true);
+        ptr<Version> new_version = make_ptr<Version>(log, pre_log, sst_set_list, lsn, db_name, file_number, true);
         return new_version;
     }
-    void VersionEdit::add_sst(ptr<class minidb::SSTable> sst, int level) {
-        add_sst_.push_back(sst);
+    void VersionEdit::add_sst(const ptr<class minidb::SSTable>& sst, int level) {
+        add_sst_[level].insert(sst);
     }
     void VersionEdit::set_log(ptr<class minidb::LogWriter> log) {
-        printf("%p",log_.get());
-        log_=log;
+        log_=std::move(log);
     }
     void VersionEdit::set_pre_log(ptr<class minidb::LogWriter> log) {
-        pre_log_=log;
+        pre_log_=std::move(log);
+    }
+    void VersionEdit::remove_sst(const ptr<class minidb::SSTable>& sst, int level) {
+        remove_sst_[level].insert(sst);
     }
 }
--- src/version.h
+++ src/version.h
@@ -12,7 +12,21 @@
 #include "file_util.h"
 #include <set>
 #include <memory>
+#include <array>
+#include <unordered_set>
 namespace minidb{
+    struct SSTableHasher{
+        size_t operator()(const ptr<SSTable>& sst)const {
+            return sst->file_number();
+        }
+    };
+    struct SSTableEqual{
+        bool operator()(const ptr<SSTable>& a,const ptr<SSTable>& b) const{
+            return a->file_number()==b->file_number();
+        }
+    };
+    using SSTableSet = std::unordered_set<ptr<SSTable>,SSTableHasher,SSTableEqual>;
+    using SstSetList = std::array<SSTableSet,config::SSTABLE_LEVEL>;
     class VersionEdit;
     class DBImpl;
     /* 1. db_impl打开version，并恢复memtable
@@ -23,13 +37,13 @@ namespace minidb{
         FileMeta filemeta;
         ptr<LogWriter> log_;
         ptr<LogWriter> pre_log_;
-        vec<ptr<SSTable>> sst_list_;
+        SstSetList sst_set_list_;
         LogSeqNumber lsn_;
         friend class VersionEdit;
         friend class DBImpl;
     public:
         //在内存中构建新的version
-        Version(ptr<LogWriter> log,ptr<LogWriter> imm_log,vec<ptr<SSTable>> sst_list,LogSeqNumber lsn,const std::string& db_name,int file_number,bool create);
+        Version(ptr<LogWriter> log,ptr<LogWriter> imm_log,SstSetList& sst_set_list,LogSeqNumber lsn,const std::string& db_name,int file_number,bool create);
         //新建version并修改version pointer
         ptr<Version> apply(ptr<VersionEdit> edit,const std::string& db_name,int file_number);
         void remove();
@@ -38,15 +52,15 @@ namespace minidb{
     class VersionEdit{
         ptr<LogWriter> log_= nullptr;
         ptr<LogWriter> pre_log_= nullptr;
-        vec<ptr<SSTable>> add_sst_;
-        vec<ptr<SSTable>> remove_sst_;
+        SstSetList add_sst_;
+        SstSetList remove_sst_;
         friend class Version;
     public:
         VersionEdit()= default;
         void set_log(ptr<LogWriter> log);
         void set_pre_log(ptr<LogWriter> log);
-        void add_sst(ptr<SSTable> sst,int level);
-        void remove_sst(ptr<SSTable> sst,int level);
+        void add_sst(const ptr<SSTable>& sst,int level);
+        void remove_sst(const ptr<SSTable>& sst,int level);
     };
 }
 #endif //MINIDB_VERSION_H
